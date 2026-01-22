fda298e8cac22e9aece67a1aced41487b22aefc7
cavit orkun <cavitorkun.gunay@vodafone.com>
2026-01-23 02:00:52 +0300

    add new directories to landing page

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,9 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
+  { href: '/blog/tech', title: 'Tech' },
+  { href: '/blog/gorusler', title: 'Görüşler' },
+  { href: '/blog/tutorials', title: 'Tutorials' },
   { href: '/tags', title: 'Tags' },
   { href: '/about', title: 'About' },
 ]
