6412e8a83901cea4a2bfb861c2b150c04693d9d0
cavit orkun <cavitorkun.gunay@vodafone.com>
2026-01-23 02:10:12 +0300

    add directories pages

---
+++ data/blog/gorusler/page.tsx
@@ -0,0 +1,31 @@
+import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
+import { allBlogs } from 'contentlayer/generated'
+import { genPageMetadata } from 'app/seo'
+import ListLayout from '@/layouts/ListLayoutWithTags'
+
+const POSTS_PER_PAGE = 5
+
+export const metadata = genPageMetadata({ title: 'Görüşler' })
+
+export default async function GoruslerPage(props: { searchParams: Promise<{ page: string }> }) {
+  const allPosts = allCoreContent(sortPosts(allBlogs))
+  // Filter posts by Gorusler category
+  const posts = allPosts.filter((post) => post.path?.startsWith('blog/gorusler/'))
+
+  const pageNumber = 1
+  const totalPages = Math.ceil(posts.length / POSTS_PER_PAGE)
+  const initialDisplayPosts = posts.slice(0, POSTS_PER_PAGE * pageNumber)
+  const pagination = {
+    currentPage: pageNumber,
+    totalPages: totalPages,
+  }
+
+  return (
+    <ListLayout
+      posts={posts}
+      initialDisplayPosts={initialDisplayPosts}
+      pagination={pagination}
+      title="Görüşler"
+    />
+  )
+}
