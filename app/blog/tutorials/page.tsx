616c512af7d174d22d3b2d2bb104b8e48857585b
cavit orkun <cavitorkun.gunay@vodafone.com>
2026-01-23 02:40:14 +0300

    page.tsx recreate for sub pages

--- app/blog/tutorials/page.tsx
+++ app/blog/tutorials/page.tsx
@@ -2,19 +2,39 @@ import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import { genPageMetadata } from 'app/seo'
 import ListLayout from '@/layouts/ListLayoutWithTags'
+import { notFound } from 'next/navigation'
 
 const POSTS_PER_PAGE = 5
 
 export const metadata = genPageMetadata({ title: 'Tutorial Posts' })
 
-export default async function TutorialsPage(props: { searchParams: Promise<{ page: string }> }) {
+export async function generateStaticParams() {
   const allPosts = allCoreContent(sortPosts(allBlogs))
-  // Filter posts by tutorials category
   const posts = allPosts.filter((post) => post.path?.startsWith('blog/tutorials/'))
-
-  const pageNumber = 1
   const totalPages = Math.ceil(posts.length / POSTS_PER_PAGE)
-  const initialDisplayPosts = posts.slice(0, POSTS_PER_PAGE * pageNumber)
+  return Array.from({ length: totalPages }, (_, i) => ({
+    page: (i + 1).toString(),
+  }))
+}
+
+export default async function TutorialsPageNumber(props: {
+  params: Promise<{ page: string }>
+}) {
+  const params = await props.params
+  const pageNumber = parseInt(params.page as string)
+  const allPosts = allCoreContent(sortPosts(allBlogs))
+  const posts = allPosts.filter((post) => post.path?.startsWith('blog/tutorials/'))
+  const totalPages = Math.ceil(posts.length / POSTS_PER_PAGE)
+
+  if (pageNumber > totalPages) {
+    return notFound()
+  }
+
+  const initialDisplayPosts = posts.slice(
+    POSTS_PER_PAGE * (pageNumber - 1),
+    POSTS_PER_PAGE * pageNumber
+  )
+
   const pagination = {
     currentPage: pageNumber,
     totalPages: totalPages,
